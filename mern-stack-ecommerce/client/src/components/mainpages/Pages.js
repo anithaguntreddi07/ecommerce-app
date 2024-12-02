4b685e8c1a392cedfec9719c128658b3e56dc400
anithaguntreddi07 <126941619+anithaguntreddi07@users.noreply.github.com>
2024-12-03 00:48:22 +0530

    Add files via upload

---
+++ mern-stack-ecommerce/client/src/components/mainpages/Pages.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Product from './products/Product';
+import Login from './login/Login';
+import Register from './login/Register';
+import Cart from './cart/Cart';
+import { Route, Routes } from 'react-router-dom';
+import DetailProduct from './utils/DetailProducts/DetailProduct';
+
+const Pages = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Product />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/detail/:id' element={<DetailProduct />} />
+    </Routes>
+  );
+};
+
+export default Pages;
+// import React from 'react'
+// import Product from './products/Product'
+// import Login from './login/Login'
+// import Register from './login/Register'
+// import Cart from './cart/Cart'
+// import { Route, Routes } from 'react-router-dom'
+// import DetailProduct from './utils/DetailProducts/DetailProduct'
+
+// const Pages = () => {
+//   return (
+//     <Routes>
+//       <Route path='/' element={<Product/>}/>
+//       <Route path='/login' element={<Login/>} />
+//       <Route path='/register' element={<Register/>}/>
+//       <Route path='/cart' element={<Cart/>}/>
+//       <Route path='/detail/:id' element={<DetailProduct/>} />
+//       <Route path='/cart' element={<Cart/>}/>
+//     </Routes>
+//   )
+// }
+
+// export default Pages
